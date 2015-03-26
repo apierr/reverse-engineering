3271d88422be45b48f8269e3fa5cb2accae30f2e
Antonio Pierro <antonio.pierro@gmail.com>
2015-03-26 16:36:48 +0100

    Generated the Entities from the metadata files

---
+++ src/Application/Multimedia/ModelBundle/Entity/Course.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Application\Multimedia\ModelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Course
+ */
+class Course
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Course
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
